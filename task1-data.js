1996f02459bfdc672870d86659c1257e506eeb36
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-08-19 21:38:14 +0100

    Create task1-data.js

---
+++ task1-data.js
@@ -0,0 +1,28 @@
+export const dataBooks = [
+  {
+    "title": "The Great Gatsby",
+    "author": "F. Scott Fitzgerald",
+    "genre": "Classic"
+  },
+  {
+    "title": "To Kill a Mockingbird",
+    "author": "Harper Lee",
+    "genre": "Classic"
+  },
+  {
+    "title": "Harry Potter and the Sorcerer's Stone",
+    "author": "J.K. Rowling",
+    "genre": "Fantasy"
+  },
+  {
+    "title": "Pride and Prejudice",
+    "author": "Jane Austen",
+    "genre": "Romance"
+  },
+  {
+    "title": "The Hobbit",
+    "author": "J.R.R. Tolkien",
+    "genre": "Fantasy"
+  }
+  // Add more books if desired
+]
