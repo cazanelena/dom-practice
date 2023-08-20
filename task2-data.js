bf21bd0e5176a57f300984df27e7c676307ce0fd
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-08-20 14:34:16 +0100

    Create task2-data.js

---
+++ task2-data.js
@@ -0,0 +1,31 @@
+export const products = [
+  {
+    name: "Product 1",
+    description: "Description for Product 1",
+    category: "Electronics",
+    imageUrl: "https://example.com/product1.jpg",
+    id: 1
+  },
+  {
+    name: "Product 2",
+    description: "Description for Product 2",
+    category: "Clothing",
+    imageUrl: "https://example.com/product2.jpg",
+    id: 2
+  },
+  {
+    name: "Product 3",
+    description: "Description for Product 3",
+    category: "Electronics",
+    imageUrl: "https://example.com/product3.jpg",
+    id: 3
+  },
+  {
+    name: "Product 4",
+    description: "Description for Product 4",
+    category: "Home & Kitchen",
+    imageUrl: "https://example.com/product4.jpg",
+    id: 4
+  },
+  // Add more products as needed
+];
